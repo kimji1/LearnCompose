ce70c2557fb06240b9f5d3a8f3cd7a8558e05895
kimji1 <dev.kimji1@gmail.com>
2023-05-17 09:43:37 +0900

    [STEP 9] 앱의 스타일 지정 및 테마 설정

--- BasicComposeCodelab/app/src/main/java/com/example/basiccomposecodelab/ui/theme/Theme.kt
+++ BasicComposeCodelab/app/src/main/java/com/example/basiccomposecodelab/ui/theme/Theme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -42,7 +45,7 @@ fun BasicComposeCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -65,6 +68,6 @@ fun BasicComposeCodelabTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
